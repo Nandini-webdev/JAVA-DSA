4a18babb057637b7cd64743160e05f68fa25f1b7
Nandini-webdev <nandinishadangi64@gmail.com>
2026-08-10 19:46:02 +0530

    Create SlidingWindow.java

---
+++ SlidingWindow.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        int n = nums.length;
+
+        int[] ans = new int[n - k + 1];
+
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // 1. Remove elements outside the window
+            while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+
+            // 2. Remove smaller elements
+            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
+                dq.pollLast();
+            }
+
+            // 3. Add current index
+            dq.offerLast(i);
+
+            // 4. Start storing answers
+            if (i >= k - 1) {
+                ans[index] = nums[dq.peekFirst()];
+                index++;
+            }
+        }
+
+        return ans;
+    }
+}
